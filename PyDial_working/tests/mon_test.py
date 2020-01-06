d2aadb6ffb0f8532c535ea658f64afa8b7bd37df
kanteao <kanteao@gmail.com>
2020-01-06 14:31:32 +0100

    finalisaion du projet

--- PyDial_working/tests/mon_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-r = requests.post('http://localhost:8082/newcall?', data='{"session":"wesh"}')
-print r
-while(True):
-	i = raw_input("Say :")
-	if (i == 'q'):
-		break
-	r = requests.post('http://localhost:8082/next?', data='{"session":"wesh", "result":{"resultType":"Partial","alts":[{"transcript":"' + i + '", "confidence":1}]} }')
-	print r.json()['text']
